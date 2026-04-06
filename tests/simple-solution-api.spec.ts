090c0db46fe3215ebca84c7cdbdc922febf4dcf2
invo518 <innavodiana.84@gmail.com>
2026-04-06 22:38:07 +0300

    HW-10 fix tests

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -1,9 +1,8 @@
 import { expect, test } from '@playwright/test'
 
 import { StatusCodes } from 'http-status-codes'
-import {describe} from 'node:test'
 
-describe('Test endpoint GET /test-orders/{id}',() => {
+test.describe('Test endpoint GET /test-orders/{id}',() => {
   const baseUrl = 'https://backend.tallinn-learning.ee/test-orders/'
 
 test('get order with correct id should receive code 200', async ({ request }) => {
@@ -11,7 +10,7 @@ test('get order with correct id should receive code 200', async ({ request }) =>
   const responseBody = await response.json()
   const statusCode = response.status()
   console.log('response body:', responseBody)
-  expect(statusCode).toBe(200)
+  expect(statusCode).toBe(StatusCodes.OK)
 })
 test('get order with incorrect id should receive code 400', async ({ request }) => {
   const response = await request.get(`${baseUrl}12`);
@@ -32,7 +31,7 @@ test('get order where id is in letters should receive 400', async ({ request }) 
   expect(statusCode).toBe(400);
 })
 })
-describe('Test endpoint PUT /test-orders/{id}',() => {
+test.describe('Test endpoint PUT /test-orders/{id}',() => {
   const baseUrl = 'https://backend.tallinn-learning.ee/test-orders/'
   const requestBody = {
     status: 'OPEN',
@@ -81,18 +80,26 @@ test('Successful update order with correct length api_key and correct id should 
       console.log('Unauthorized',statusCode)
       expect(statusCode).toBe(400)
     })
-})
-describe('Test endpoint Delete/test-orders/{id}',() => {
-  const baseUrl = 'https://backend.tallinn-learning.ee/test-orders/'
 
   test('Delete order with correct id should return 204', async ({ request }) => {
     const requestHeaders = { api_key: '1234566745347856' };
-    const response = await request.delete(`${baseUrl}1`, {headers: requestHeaders})
+    const response = await request.delete(`${baseUrl}1`, {
+      headers: requestHeaders
+    })
+
     const statusCode = response.status()
-    console.log('Order deleted successfully', statusCode)
+    console.log('Order successful delete', statusCode)
     expect(statusCode).toBe(204)
 
   })
+  test('Check Deleted order should return 200 Not Found', async ({ request }) => {
+    const response = await request.get(`${baseUrl}1`)
+    const statusCode = response.status()
+    expect(response.status()).toBe(200)
+    console.log("Order not found",statusCode)
+
+  })
+
   test('Not successfully delete order with incorrect length api_key should return 401', async ({ request }) => {
     const requestHeaders = { api_key: '1234' };
     const response = await request.delete(`${baseUrl}1`, {headers: requestHeaders})
